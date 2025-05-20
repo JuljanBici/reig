eee741d577d95561425f9dec4cfd4673db4f062c
JuljanBici <julianbici14@gmail.com>
2025-05-20 11:13:04 +0200

    minor fixes

--- app/components/CategorySection.js
+++ app/components/CategorySection.js
@@ -1,7 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
 
-export default function CategorySection({ products, category }) {
+export default function categorySection({ products, category }) {
   return (
     <>
       <section> 
